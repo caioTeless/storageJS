ad3747e85828cda5599612d4539a4082a79ae7aa
Caio Teles <caioo.teless@gmail.com>
2024-08-05 23:03:58 -0300

    validations

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,40 @@ import Modal from "./modules/modal.js";
 
 const app = new Vue({
   el: "#app",
+  data: {
+    people: {
+      name: {
+        necessary: true,
+        value: "",
+        error: "",
+      },
+      postalCode: {
+        necessary: true,
+        value: "",
+        error: "",
+      },
+      address: {
+        necessary: true,
+        value: "",
+        error: "",
+      },
+      city: {
+        necessary: true,
+        value: "",
+        error: "",
+      },
+      phone: {
+        necessary: false,
+        value: "",
+        error: "",
+      },
+      type: {
+        necessary: true,
+        value: "",
+        error: "",
+      },
+    },
+  },
   methods: {
     loadNav() {
       fetch("_navbar.html")
@@ -15,6 +49,32 @@ const app = new Vue({
         .catch((error) => {
           console.log("Ocorreu algum erro");
         });
+    },
+    validateForm() {
+      this.validFields();
+    },
+    validFields() {
+      for (let field in this.people) {
+        if (
+          this.people[field].necessary &&
+          this.people[field].value.trim() === ""
+        ) {
+          this.people[field].error = "É necessário o preenchimento.";
+        } else {
+          this.people[field].error = "";
+        }
+      }
+    },
+
+    validCep(cep) {
+      var re = /^\d{5}-?\d{3}$/;
+      return re.test(cep);
+    },
+
+    validEmail(email) {
+      var re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
     },
   },
   mounted() {
